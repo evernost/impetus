90fac2d572bf15217daa6dfe81110ab98cfaff48
qubi <nitrogenium@outlook.fr>
2026-09-15 21:36:02 +0200

    rewrote the core with a more suited format for the big numbers

--- random/solver.c
+++ random/solver.c
@@ -26,9 +26,8 @@
 #include <string.h>
 #include <time.h>       // For random numbers
 
-// Max number of digits supported for the target
-// It can certainly be extended up to 65535 without
-// too much issue.
+// Max number of digits that can be manipulated.
+// It can certainly be extended up to 65535 without too much trouble.
 // Beyond that, there will be issues (size is stored in 'uint16_t')
 #define MAX_DIGITS 512
 
@@ -38,19 +37,32 @@
 
 
 
+// Define a "MegaNum(C)" type for big fat numbers
+typedef struct
+{
+  uint8_t digits[MAX_DIGITS];
+  uint16_t len;
+} MegaNum_t;
+
+
+
 // ============================================================================
 // PROTOTYPES
 // ============================================================================
-int parseArgs(int argc, char *argv[], uint8_t* target, uint16_t target_len);
-void initMultiplicand(uint8_t* x, uint16_t target_len);
+int parseArgs(int argc, char *argv[], MegaNum_t* x);
 
-void mult(uint8_t* a, uint16_t len_a, uint8_t* b, uint16_t len_b, uint8_t* y);
-void _multTable(uint8_t table[10][MAX_DIGITS], uint8_t* a, uint16_t len_a);
-void _multAccumulate(uint8_t* acc, uint8_t table[10][MAX_DIGITS], uint16_t tableLen, uint8_t index, uint16_t shift);
+int megaNum_updateLen(MegaNum_t* x);
+int megaNum_randInit(MegaNum_t* x, uint16_t size);
+int megaNum_mult(MegaNum_t* a, MegaNum_t* b, MegaNum_t* y);
+
+int _multMakeTable(MegaNum_t table[9], MegaNum_t* x);
+int _multAccumulate(MegaNum_t* accu, MegaNum_t a_multTable[9], MegaNum_t* b, uint16_t b_digitPtr);
+
+
 
 uint16_t getLen(uint8_t* x);
-void score(uint8_t* x, uint8_t* y, uint16_t target_len);
-void print(uint8_t* x, uint16_t len_x);
+uint16_t score(MegaNum_t x, MegaNum_t y, MegaNum_t target);
+void print(MegaNum_t x);
 
 
 
@@ -60,52 +72,40 @@ void print(uint8_t* x, uint16_t len_x);
 int main(int argc, char *argv[])
 {
   int ret;
-  uint8_t a[MAX_DIGITS] = {0};
-  uint8_t b[MAX_DIGITS] = {0};
-  uint8_t res[MAX_DIGITS] = {0};
+  MegaNum_t a = {.digits = {0}, .len = 1};
+  MegaNum_t b = {.digits = {0}, .len = 1};
+  MegaNum_t prod = {.digits = {0}, .len = 1};
 
   // Default target number
   // Endianness: just type it like you would type it naturally.
-  uint8_t target[MAX_DIGITS] = {1,0,0,0,0,0,9,8,0,0,0,1,5,0,1};
-  uint32_t target_len = 15;
+  // EXAMPLE: below the number 100000980001501
+  MegaNum_t target = {.digits = {1,0,0,0,0,0,9,8,0,0,0,1,5,0,1}, .len = 15};
 
   // --------------------------------------------------------------------------
-  // PARSE THE INPUT NUMBER
+  // PARSE THE INPUT
   // --------------------------------------------------------------------------
-  ret = parseArgs(argc, argv, target, target_len);
+  ret = parseArgs(argc, argv, &target);
   if (ret != EXIT_SUCCESS) return ret;
-
-  // Reverse input endianness: index 0 is the lowest significant digit.
-  // Reverse is done using XOR swapping
-  for(uint32_t i = 0; i < (target_len/2); i++)
-  {
-    target[i]                   ^= target[(target_len-1) - i];
-    target[(target_len-1) - i]  ^= target[i];
-    target[i]                   ^= target[(target_len-1) - i];
-  }
 
   // --------------------------------------------------------------------------
   // SEARCH INIT
   // --------------------------------------------------------------------------
   srand((unsigned)time(NULL));
-  initMultiplicand(a, target_len);
-  initMultiplicand(b, target_len);
+  megaNum_randInit(&a, target.len/2);
+  megaNum_randInit(&b, target.len/2);
 
   // --------------------------------------------------------------------------
-  // SEARCH MAIN LOOP
+  // SEARCH LOOP
   // --------------------------------------------------------------------------
-  print(a, 32);
-  print(b, 32);
-  printf("%d\n", getLen(a));
-  mult(a, 15, b, 15, res);
+  print(a);
+  print(b);
+  megaNum_mult(&a, &b, &prod);
 
   // --------------------------------------------------------------------------
   // SEARCH RESULT
   // --------------------------------------------------------------------------
-  print(res, 32);
-  print(target, 32);
-
-
+  print(prod);
+  print(target);
 
 
   return EXIT_SUCCESS;
@@ -113,21 +113,24 @@ int main(int argc, char *argv[])
 
 
 
-int parseArgs(int argc, char *argv[], uint8_t* target, uint16_t target_len)
+int parseArgs(int argc, char *argv[], MegaNum_t* x)
 {
+  // No argument
   if (argc == 1)
   {
     printf("[DEBUG] No input specified, using the default target.\n");
-    return EXIT_SUCCESS;
   }
+
+  // The target number is explicitely specified
   else if (argc == 2)
   {
-    for(uint32_t i = 0; i < MAX_DIGITS; i++)
+    // Read the input and fill the target
+    for(uint16_t i = 0; i < MAX_DIGITS; i++)
     {
       if (argv[1][i] == '\0')
       {
         printf("[DEBUG] Your input has %" PRIu32 " digit%s.\n", i, i > 1 ? "s" : "");
-        target_len = i;
+        x->len = i;
         break;
       }
       else if ((i == (MAX_DIGITS-1)) && (argv[1][i+1] != '\0'))
@@ -137,20 +140,29 @@ int parseArgs(int argc, char *argv[], uint8_t* target, uint16_t target_len)
       }
       else
       {
-        target[i] = argv[1][i] - '0';
+        x->digits[i] = argv[1][i] - '0';
       }
     }
-
-    // Fill the leading digits with zeros
-    for(uint32_t i = target_len; i < MAX_DIGITS; i++)
-    {
-      target[i] = 0;
-    }
   }
+
+  // Anything else is invalid
   else if (argc > 2)
   {
     printf("Usage: %s [input_number]\n", argv[0]);
     return EXIT_FAILURE;
+  }
+
+
+
+  // Reverse input endianness.
+  // For simplicity in the processing, we want index 0 to be the lowest significant digit
+  // which is the opposite of a natural input.
+  // Reverse is done using XOR swapping
+  for(uint16_t i = 0; i < ((x->len)/2); i++)
+  {
+    x->digits[i]                 ^= x->digits[((x->len)-1) - i];
+    x->digits[((x->len)-1) - i]  ^= x->digits[i];
+    x->digits[i]                 ^= x->digits[((x->len)-1) - i];
   }
 
   return EXIT_SUCCESS;
@@ -158,106 +170,166 @@ int parseArgs(int argc, char *argv[], uint8_t* target, uint16_t target_len)
 
 
 
-void mult(uint8_t* a, uint16_t len_a, uint8_t* b, uint16_t len_b, uint8_t* y)
+int megaNum_updateLen(MegaNum_t* x)
 {
-  uint8_t table[10][MAX_DIGITS] = {0};
+  uint16_t i = MAX_DIGITS-1;
 
-  // Generate the list of multiples of 'a'
-  _multTable(table, a, len_a);
-  
-  for(uint16_t i = 0; i < len_b; i++)
+  while(x->digits[i] == 0)
   {
-    if (b[i] != 0) _multAccumulate(y, table, len_a, b[i], i);
+    if (i == 0)
+    {
+      x->len = 1;
+      return EXIT_SUCCESS;
+    }
+    i--;
   }
+
+  x->len = i+1;
+  return EXIT_SUCCESS;
 }
 
 
 
-void _multTable(uint8_t table[10][MAX_DIGITS], uint8_t* a, uint16_t len_a)
+int megaNum_randInit(MegaNum_t* x, uint16_t size)
 {
-  // Multiply 'a' by integers from 1 to 9
+  for(uint16_t i = 0; i < size; i++)
+  {
+    x->digits[i] = (uint8_t)(rand() % 10);
+  }
+  megaNum_updateLen(x);
+
+  return EXIT_SUCCESS;
+}
+
+
+
+int megaNum_mult(MegaNum_t* a, MegaNum_t* b, MegaNum_t* y)
+{
+  int ret;
+  MegaNum_t a_multTable[9];
+
+  // Generate the list of multiples of 'a'
+  _multMakeTable(a_multTable, a);
+  
+  // Do the actual multiplication
+  for(uint16_t i = 0; i < (b->len); i++)
+  {
+    ret = _multAccumulate(y, a_multTable, b, i);
+
+    if (ret != EXIT_SUCCESS) return ret;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+
+
+int _multMakeTable(MegaNum_t table[9], MegaNum_t* x)
+{
+  // Multiply 'x' by integers from 1 to 9
   for(uint8_t i = 1; i < 10; i++)
   {
     uint8_t prod;
     uint8_t carry = 0;
 
-    for(uint16_t j = 0; j < len_a; j++)
+    // Create a local alias for the current multiple.
+    // NOTE: the array stores only 9 multiples because multiples
+    // of '0' are trivial.
+    MegaNum_t *elem = &table[i-1];
+
+    // By default, assume that the i-th multiple of 'x' has
+    // as many digits as 'x'.
+    elem->len = x->len;
+
+    for(uint16_t d = 0; d < (x->len); d++)
     {
-      prod = (a[j]*i) + carry;
-      table[i][j] = prod % 10;
-      carry = prod / 10;
+      prod = ((x->digits[d])*i) + carry;
+
+      elem->digits[d] = prod % 10;
+      carry           = prod / 10;
     }
     
-    table[i][len_a] = carry;
-    
-    // printf("mult = %d\n", i);
-    // print(table[i], len_a+1);
+    if (carry != 0)
+    {
+      elem->digits[x->len] = carry;
+      elem->len++;
+    }
   }
+
+  return EXIT_SUCCESS;
 }
 
 
 
-void _multAccumulate(uint8_t* acc, uint8_t table[10][MAX_DIGITS], uint16_t table_len, uint8_t index, uint16_t shift)
+int _multAccumulate(MegaNum_t* accu, MegaNum_t a_multTable[9], MegaNum_t* b, uint16_t b_digitPtr)
 {
+  uint16_t shift;
+  uint8_t index;
+  uint8_t multiplier;
   uint8_t carry = 0;
   uint8_t sum;
 
-  if ((shift + table_len) >= MAX_DIGITS)
+  if (b_digitPtr >= (b->len))
   {
-    printf("[ERROR] _multAccumulate(): unhandled exception (input possibly too big)\n");
-    exit(EXIT_FAILURE);
+    printf("[ERROR] _multAccumulate(): 'b_digitPtr' points outside the length of 'b'. Possible internal error, sorry for that.\n");
+    return EXIT_FAILURE;
   }
 
-  // NOTE: the '+1' in 'table_len+1' is to account for a possible carry.
-  for(uint16_t i = 0; i < (table_len + 1); i++)
+  // Multiply by 0: nothing to do
+  if (b->digits[b_digitPtr] == 0)
   {
-    sum = acc[shift + i] + table[index][i] + carry;
-
-    acc[shift + i] = sum % 10;
-    carry = sum / 10;
+    return EXIT_SUCCESS;
   }
+
+  // Aliases
+  shift       = b_digitPtr;
+  multiplier  = b->digits[b_digitPtr];
+  index       = multiplier-1;
+
+  // Add the product a*b[i] to the accumulator at the right place (shifted 'i' times)
+  for(uint16_t d = 0; d < (a_multTable[index].len); d++)
+  {
+    sum = accu->digits[shift + d] + a_multTable[index].digits[d] + carry;
+
+    accu->digits[shift + d] = sum % 10;
+    carry                   = sum / 10;
+  }
+
+  if (carry != 0)
+  {
+    accu->digits[shift + a_multTable[multiplier-1].len]++;
+    accu->len = shift + a_multTable[multiplier-1].len + 1;
+  }
+  else
+  {
+    accu->len = shift + a_multTable[multiplier-1].len;
+  }
+
+  return EXIT_SUCCESS;
 }
 
 
 
-void initMultiplicand(uint8_t* x, uint16_t target_len)
+uint16_t score(MegaNum_t x, MegaNum_t y, MegaNum_t target)
 {
-  for(uint16_t i = 0; i < (target_len/2); i++)
-  {
-    x[i] = (uint8_t)(rand() % 10);
-  }
+  return 0;
 }
 
 
 
-uint16_t getLen(uint8_t* x)
+void print(MegaNum_t x)
 {
-  uint16_t i = MAX_DIGITS-1;
+  // for(uint16_t i = x.len; i-- > 0; )
+  // {
+  //   printf("%d%s", x.digits[i], i == 0 ? "\n" : "");
+  // }
 
-  while(x[i] == 0)
+  for(uint16_t i = x.len; i-- > 0; )
   {
-    if (i == 0) return 0;
-    i--;
+    printf("%d", x.digits[i]);
   }
 
-  return (i+1);
-}
-
-
-
-void score(uint8_t* x, uint8_t* y, uint16_t target_len)
-{
-
-}
-
-
-
-void print(uint8_t* x, uint16_t len_x)
-{
-  for(uint16_t i = len_x; i-- > 0; )
-  {
-    printf("%d%s", x[i], i == 0 ? "\n" : "");
-  }
+  printf(" (len = %d)\n", x.len);
 }
 
 
